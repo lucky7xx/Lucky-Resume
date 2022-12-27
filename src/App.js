5124e7ac9f3844d86760de54eaf1ef612563cd4b
Lucky Patel <lbp7198@gmail.com>
2022-12-27 12:17:33 +0530

    2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      Hello World this is lucky resume in raect .. testing
+    </div>
+  );
 }
 
 export default App;
