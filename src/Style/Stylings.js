3a7495b08dabd723b7044ef71b76ac16f1503071
Lucky Patel <lbp7198@gmail.com>
2022-12-28 11:26:49 +0530

    1

--- src/Style/Stylings.js
+++ src/Style/Stylings.js
@@ -17,8 +17,8 @@ const ContainerDiv = styled.div`
   backdrop-filter: blur(3px);
   box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px,
     rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
-  @media (min-width: 0px) and (max-width: 480px) {
-    width: 450px;
+  @media (min-width: 0px) and (max-width: 384px) {
+    width: 353px;
     height: auto;
     display: grid;
     grid-template-rows: repeat(1, 3fr);
@@ -36,7 +36,7 @@ const InnerDiv = styled.div`
   text-align: justify;
   justify-content: center;
   align-items: center;
-  @media (min-width: 0px) and (max-width: 480px) {
+  @media (min-width: 0px) and (max-width: 384px) {
     text-align: justify;
     font-size: 10px;
     line-height: 12px;
@@ -51,7 +51,7 @@ const ImgDiv = styled.img`
   height: 300px;
   width: 300px;
   border-radius: 30px;
-  @media (min-width: 0px) and (max-width: 480px) {
+  @media (min-width: 0px) and (max-width: 384px) {
     width: 225px;
     height: 225px;
     margin-right: auto;
@@ -76,9 +76,16 @@ const HomeDiv = styled.div`
 
 const HomepageDiv = styled.div`
   text-align: justify;
-  margin: 40px 100px;
+  /* margin: 40px 100px; */
+  margin-left: 20px;
+  margin-right: 20px;
   color: white;
   font-size: 20px;
+  @media (min-width: 0px) and (max-width: 384px) {
+    font-size: 15px;
+    margin-left: 20px;
+    margin-right: 20px;
+  }
 `;
 
 const ButtonDiv = styled.div`
@@ -90,7 +97,7 @@ const IconWrapperDiv = styled.div`
   font-size: 80px;
   display: inline-flex;
   flex-direction: row;
-  @media (min-width: 0px) and (max-width: 480px) {
+  @media (min-width: 0px) and (max-width: 384px) {
     font-size: 40px;
   }
 `;
